78c2890681d04f5304a926e8a29fc3606f3d038f
Diego Maia <ovrdoz@gmail.com>
2023-11-11 17:26:16 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,5 +3,8 @@
 # brew install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "write homebrew to zprofile"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/maia/.zprofile
+
+echo "set shellenv for homebrew"
 eval "$(/opt/homebrew/bin/brew shellenv)"
